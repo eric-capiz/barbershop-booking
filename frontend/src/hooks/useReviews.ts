953557db54c983708210b051fd899feb8d63f0c6
eric capiz <ericcapiz@gmail.com>
2025-01-13 17:48:29 -0600

    changes

--- frontend/src/hooks/useReviews.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { reviewService } from "@/services/review.service";
-
-export const useReviews = () => {
-  return useQuery({
-    queryKey: ["reviews"],
-    queryFn: reviewService.getReviews,
-  });
-};
--- frontend/src/types/admin/services.types.ts
+++
@@ -1,9 +0,0 @@
-export interface Service {
-  _id: string;
-  name: string;
-  description: string;
-  duration: number;
-  price: number;
-  category: string;
-  isActive: boolean;
-}
